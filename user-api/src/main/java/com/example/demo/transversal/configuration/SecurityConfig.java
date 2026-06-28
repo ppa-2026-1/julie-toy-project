923e07b35b9cb6eba457b54e08b2fcf8211044a3
juliemartins1 <2024013090@aluno.riogrande.ifrs.edu.br>
2026-06-27 21:36:41 -0300

    trabalho 3

--- user-api/src/main/java/com/example/demo/transversal/configuration/SecurityConfig.java
+++ user-api/src/main/java/com/example/demo/transversal/configuration/SecurityConfig.java
@@ -2,23 +2,32 @@ package com.example.demo.transversal.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.demo.transversal.auth.AuthFilter;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthFilter authFilter) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**").permitAll() 
-                        .anyRequest().permitAll())
+                        .requestMatchers(HttpMethod.POST, "/auth/login", "/api/v1/users").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
                 .headers(headers -> headers
-                .frameOptions(cust -> cust.sameOrigin()))
+                        .frameOptions(cust -> cust.sameOrigin()))
                 .httpBasic(basic -> basic.disable())
                 .formLogin(login -> login.disable())
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
