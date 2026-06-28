eb326a0a4958522757fb0724604933402a3a5c7b
Julie Martins  Monteiro <166827680+juliemartins1@users.noreply.github.com>
2026-06-27 21:24:33 -0300

    Delete notification-api directory

--- notification-api/src/main/java/com/example/demo/model/dto/NewNotificationDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model.dto;
-
-import java.util.List;
-
-public record NewNotificationDTO(
-        List<String> destinatarios,
-        String assunto,
-        String mensagem
-) {}
