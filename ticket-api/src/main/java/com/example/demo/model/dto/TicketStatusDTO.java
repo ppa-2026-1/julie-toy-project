3351b8d175f257aa25cb13702192cc972bdc95a3
Julie Martins  Monteiro <166827680+juliemartins1@users.noreply.github.com>
2026-06-27 21:25:15 -0300

    Delete ticket-api directory

--- ticket-api/src/main/java/com/example/demo/model/dto/TicketStatusDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model.dto;
-
-import com.example.demo.repository.entity.Ticket.Status;
-
-public record TicketStatusDTO(
-    Status status,
-    String responsavel,
-    String motivo
-) {}
